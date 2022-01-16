21159436d7ecd2b93436efdd3a9dfe940bc6c484
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-16 15:22:16 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(action = "roller-skate"){
+    return `This Saturday, I want to ${action}!`
+}
+saturdayFun();
+
+const mondayWork = function(action = "go to the office") {
+    return `This Monday, I will ${action}.` 
+} ;
+
+mondayWork();
+
+function wrapAdjective (vf){
+    return function(parameter = "special") {
+        return `You are ${vf}${parameter}${vf}!`;
+    }
+}
+
+wrapAdjective();
